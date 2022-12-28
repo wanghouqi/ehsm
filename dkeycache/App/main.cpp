12168040b5a35aee81b0f44395dd86d92f8c01e4
wanghouqi <houqix.wang@intel.com>
2022-12-28 14:17:51 +0800

    Fix dkeycache cup
    Signed-off-by: wanghouqi <houqix.wang@intel.com>

--- dkeycache/App/main.cpp
+++ dkeycache/App/main.cpp
@@ -23,6 +23,7 @@
 #include "la_task.h"
 #include "la_server.h"
 #include "auto_version.h"
+#include <thread>
 
 #define __STDC_FORMAT_MACROS
 #define ENCLAVE_PATH "libenclave-ehsm-dkeycache.signed.so"
@@ -32,17 +33,17 @@ sgx_enclave_id_t g_enclave_id;
 
 void ocall_print_string(uint32_t log_level, const char *str, const char *filename, uint32_t line)
 {
-    switch (log_level) 
+    switch (log_level)
     {
-        case LOG_INFO:
-        case LOG_DEBUG:
-        case LOG_ERROR:
-        case LOG_WARN:
-            log_c(log_level, str, filename, line);
-            break;
-        default:
-            log_c(LOG_ERROR, "log system error in ocall print.\n", filename, line);
-            break;
+    case LOG_INFO:
+    case LOG_DEBUG:
+    case LOG_ERROR:
+    case LOG_WARN:
+        log_c(log_level, str, filename, line);
+        break;
+    default:
+        log_c(LOG_ERROR, "log system error in ocall print.\n", filename, line);
+        break;
     }
 }
 
@@ -164,12 +165,70 @@ static void parse_args(int argc, char *argv[])
     }
 }
 
+typedef struct CPUPACKED
+{
+    char name[20];       // 定义一个char类型的数组名name有20个元素
+    unsigned int user;   // 定义一个无符号的int类型的user
+    unsigned int nice;   // 定义一个无符号的int类型的nice
+    unsigned int system; // 定义一个无符号的int类型的system
+    unsigned int idle;   // 定义一个无符号的int类型的idle
+    unsigned int lowait;
+    unsigned int irq;
+    unsigned int softirq;
+} CPU_OCCUPY;
+
+double getCpuUse(CPU_OCCUPY *o, CPU_OCCUPY *n)
+{
+    unsigned long od, nd;
+    od = (unsigned long)(o->user + o->nice + o->system + o->idle + o->lowait + o->irq + o->softirq); // 第一次(用户+优先级+系统+空闲)的时间再赋给od
+    nd = (unsigned long)(n->user + n->nice + n->system + n->idle + n->lowait + n->irq + n->softirq); // 第二次(用户+优先级+系统+空闲)的时间再赋给od
+    double sum = nd - od;
+    double idle = n->idle - o->idle;
+    return (sum - idle) / sum;
+}
+
+void printCpuUse(std::string prefix, bool onWhile)
+{
+    CPU_OCCUPY old_cpu_occupy;
+    do
+    {
+        FILE *fd;       // 定义打开文件的指针
+        char buff[256]; // 定义个数组，用来存放从文件中读取CPU的信息
+        CPU_OCCUPY cpu_occupy;
+        std::string cpu_use = "";
+
+        fd = fopen("/proc/stat", "r");
+
+        if (fd != NULL)
+        {
+            // 读取第一行的信息，cpu整体信息
+            fgets(buff, sizeof(buff), fd);
+            if (strstr(buff, "cpu") != NULL) // 返回与"cpu"在buff中的地址，如果没有，返回空指针
+            {
+                // 从字符串格式化输出
+                sscanf(buff, "%s %u %u %u %u %u %u %u", cpu_occupy.name, &cpu_occupy.user, &cpu_occupy.nice, &cpu_occupy.system, &cpu_occupy.idle, &cpu_occupy.lowait, &cpu_occupy.irq, &cpu_occupy.softirq);
+                // cpu的占用率 = （当前时刻的任务占用cpu总时间-前一时刻的任务占用cpu总时间）/ （当前时刻 - 前一时刻的总时间）
+                cpu_use = std::to_string(getCpuUse(&old_cpu_occupy, &cpu_occupy) * 100) + "%";
+                old_cpu_occupy = cpu_occupy;
+            }
+        }
+        log_w("========> %s getCpuUse = %s", prefix.c_str(), cpu_use.c_str());
+        // log_w("========> getCpuUse = %s", cpu_use.c_str());
+        if (onWhile)
+        {
+            sleep(1); // 延时1s；
+        }
+    } while (onWhile);
+}
+
 int main(int argc, char *argv[])
 {
     // mkdir RUNTIME_FOLDER
-    if (access(RUNTIME_FOLDER, F_OK) != 0) {
+    if (access(RUNTIME_FOLDER, F_OK) != 0)
+    {
         printf("Initializing runtime folder [path: %s].\n", RUNTIME_FOLDER);
-        if (mkdir(RUNTIME_FOLDER, 0755) != 0) {
+        if (mkdir(RUNTIME_FOLDER, 0755) != 0)
+        {
             printf("Create runtime folder failed!\n");
             return -1;
         }
@@ -179,9 +238,11 @@ int main(int argc, char *argv[])
     log_i("Service name:\t\tDomainKey Caching Service %s", EHSM_VERSION);
     log_i("Service built:\t\t%s", EHSM_DATE);
     log_i("Service git_sha:\t\t%s", EHSM_GIT_SHA);
+    printCpuUse("end init logger", false);
 
     // process argv
     parse_args(argc, argv);
+    printCpuUse("end parse args", false);
 
     log_i("Runtime folder:\t\t%s", RUNTIME_FOLDER);
     log_i("DomainKey Server IP:\t\t%s", deploy_ip_addr.c_str());
@@ -199,6 +260,7 @@ int main(int argc, char *argv[])
         log_e("failed(%d) to create enclave.\n", ret);
         return -1;
     }
+    printCpuUse("end create enclave", false);
 
     // Connect to the dkeyserver and retrieve the domain key via the remote secure channel
     log_i("Host: launch TLS client to initiate TLS connection\n");
@@ -208,6 +270,7 @@ int main(int argc, char *argv[])
         log_e("failed to initialize the dkeycache service.\n");
         sgx_destroy_enclave(g_enclave_id);
     }
+    printCpuUse("end Connect dkeyserver", false);
 
     // create server instance, it would listen on sockets and proceeds client's requests
     g_la_task = new (std::nothrow) LaTask;
@@ -215,14 +278,18 @@ int main(int argc, char *argv[])
 
     if (!g_la_task || !g_la_server)
         return -1;
+    printCpuUse("end new task and server", false);
 
     atexit(cleanup);
+    printCpuUse("end atexit", false);
 
     // register signal handler so to respond to user interception
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
+    printCpuUse("end signal", false);
 
     g_la_task->start();
+    printCpuUse("end task start", false);
 
     if (g_la_server->init() != 0)
     {
@@ -231,10 +298,11 @@ int main(int argc, char *argv[])
     else
     {
         log_i("dkeycache service is ON...\n");
-        // printf("Press Ctrl+C to exit...\n");
+        std::thread(printCpuUse, "end server init", true).detach();
+        log_i("Press Ctrl+C to exit...\n");
         g_la_server->doWork();
     }
-    
+
     logger_shutDown();
 
     sgx_destroy_enclave(g_enclave_id);
